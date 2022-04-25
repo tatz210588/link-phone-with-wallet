d049b6bec8f56e06d0c527c8d7315f88fe2edd4b
Tamal Patra <patra.tamal@gmail.com>
2022-04-25 21:11:18 +0530

    - style refactoring - first dose of tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+const Document = () => {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default Document
