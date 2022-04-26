3dc903b2cadd08f5041cfe7f6c774cde9977a76b
Tamal Patra <patra.tamal@gmail.com>
2022-04-26 21:12:56 +0530

    Introducing `Container` component

---
+++ components/Container.tsx
@@ -0,0 +1,24 @@
+import { NextPage } from 'next'
+
+type ContainerProps = {
+  wrapperClass: string
+  containerClass: string
+  contentWrapperClass: string
+}
+
+const Container: NextPage<ContainerProps> = ({
+  children,
+  wrapperClass = 'wrapper',
+  containerClass = 'container',
+  contentWrapperClass = 'content-wrapper',
+}) => (
+  <>
+    <div className={wrapperClass}>
+      <div className={containerClass}>
+        <div className={contentWrapperClass}>{children}</div>
+      </div>
+    </div>
+  </>
+)
+
+export default Container
--- components/Hero.js
+++ components/Hero.js
@@ -2,78 +2,81 @@ import React from 'react'
 import Router from 'next/router'
 import Home from '../assets/home.png'
 import Image from 'next/image'
-import logo from "../assets/QR.png"
-
+import logo from '../assets/QR.png'
+import Container from './Container'
 
 const style = {
-    wrapper: `relative`,
-    container: `before:content-[''] before:bg-red-500 before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0 before:bg-[url('../assets/4.jpg')] before:bg-cover before:bg-center before:bg-fixed before:opacity-100 before:blur`,
-    contentWrapper: `flex h-screen relative justify-center flex-wrap items-center`,
-    copyContainer: `w-1/2`,
-    title: `relative text-[#277cc2] text-[46px] font-semibold`,
-    description: `text-[#fffbeb] container-[400px] text-xl mt-[0.8rem] mb-[2.5rem]`,
-    ctaContainer: `flex`,
-    qrButton: ` relative text-lg font-semibold bg-[#2181e2] rounded-lg mr-5 text-white cursor-pointer`,
-    accentedButton: ` relative text-lg font-semibold px-12 py-4 bg-[#2181e2] rounded-lg mr-5 text-white hover:bg-[#42a0ff] cursor-pointer`,
-    button: ` relative text-lg font-semibold px-12 py-4 bg-[#363840]  rounded-lg mr-5 text-[#e4e8ea] hover:bg-[#4c505c] cursor-pointer`,
-    cardContainer: `rounded-[7rem]`,
-    infoContainer: `h-20 bg-[#313338] p-4 rounded-b-lg flex items-center text-white`,
-    author: `flex flex-col justify-center ml-4`,
-    name: ``,
-    infoIcon: `flex justify-end items-center flex-1 text-[#8a939b] text-3xl font-bold`,
+  // wrapper: `relative`,
+  // container: `before:content-[''] before:bg-red-500 before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0 before:bg-[url('../assets/4.jpg')] before:bg-cover before:bg-center before:bg-fixed before:opacity-100 before:blur`,
+  // contentWrapper: `flex h-screen relative justify-center flex-wrap items-center`,
+  copyContainer: `w-1/2`,
+  title: `relative text-[#277cc2] text-[46px] font-semibold`,
+  description: `text-[#fffbeb] container-[400px] text-xl mt-[0.8rem] mb-[2.5rem]`,
+  ctaContainer: `flex`,
+  qrButton: ` relative text-lg font-semibold bg-[#2181e2] rounded-lg mr-5 text-white cursor-pointer`,
+  accentedButton: ` relative text-lg font-semibold px-12 py-4 bg-[#2181e2] rounded-lg mr-5 text-white hover:bg-[#42a0ff] cursor-pointer`,
+  button: ` relative text-lg font-semibold px-12 py-4 bg-[#363840]  rounded-lg mr-5 text-[#e4e8ea] hover:bg-[#4c505c] cursor-pointer`,
+  cardContainer: `rounded-[7rem]`,
+  infoContainer: `h-20 bg-[#313338] p-4 rounded-b-lg flex items-center text-white`,
+  author: `flex flex-col justify-center ml-4`,
+  name: ``,
+  infoIcon: `flex justify-end items-center flex-1 text-[#8a939b] text-3xl font-bold`,
 }
 
-
-
 const Hero = () => {
+  return (
+    <Container>
+      <div className={`${style.copyContainer} `}>
+        <div className={style.title}>Send crypto to phone number</div>
+        <div className={style.description}>
+          Forget the era of wallet address and send crypto directly to phone
+          number now.
+        </div>
+        <div className={style.ctaContainer}>
+          <button
+            className={style.accentedButton}
+            onClick={() => {
+              Router.push({
+                pathname: '/pay',
+              })
+            }}
+          >
+            Pay to Phone
+          </button>
 
-    return (
-        <div className={style.wrapper}>
+          <button
+            className={style.button}
+            onClick={() => {
+              Router.push({ pathname: '/register' })
+            }}
+          >
+            Sign up
+          </button>
+          <button className={style.button}>Send NFT</button>
+        </div>
+      </div>
+      <div className={`${style.cardContainer} `}>
+        <Image className="w-1/2 rounded-t-lg " src={Home} alt="" />
 
-            <div className={style.container}>
-                <div className={`${style.contentWrapper}`}>
-                    <div className={`${style.copyContainer} `}>
-                        <div className={style.title}>
-                            Send crypto to phone number
-                        </div>
-                        <div className={style.description}>
-                            Forget the era of wallet address and send crypto directly to phone number now.
-                        </div>
-                        <div className={style.ctaContainer}>
-                            <button className={style.accentedButton} onClick={() => {
-                                Router.push({
-                                    pathname: '/pay'
-                                })
-                            }}>Pay to Phone</button>
-
-
-
-                            <button className={style.button} onClick={() => { Router.push({ pathname: '/register' }) }}>Sign up</button>
-                            <button className={style.button}>Send NFT</button>
-                        </div>
-                    </div>
-                    <div className={`${style.cardContainer} `}>
-                        <Image className="rounded-t-lg w-1/2 " src={Home} alt="" />
-
-                        <div className={style.infoContainer}>
-                            <img
-                                className="h-[2.5rem] rounded-full"
-                                src='https://lh3.googleusercontent.com/hKfUfZcOpvGfsNz4p6Pu9b2ckD4MRFcL-XbwPuZ2PwtOynFloE85nekShDqAZcN0JCFBAXPX_royc9Lx1k_gAJJmqJLyoPhnvjWi4g=s80'
-                                alt=""
-                            />
-                            <div className={style.author}>
-                                <div className={style.name}>Tatz</div>
-                                <a className="text-[#1868b7]"
-                                    href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/43617070992181872498657409852160512582181349889597339264956620564284712157185">
-                                    Tathagat
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div >
-    )
+        <div className={style.infoContainer}>
+          <img
+            className="h-[2.5rem] rounded-full"
+            src="https://lh3.googleusercontent.com/hKfUfZcOpvGfsNz4p6Pu9b2ckD4MRFcL-XbwPuZ2PwtOynFloE85nekShDqAZcN0JCFBAXPX_royc9Lx1k_gAJJmqJLyoPhnvjWi4g=s80"
+            alt=""
+          />
+          <div className={style.author}>
+            <div className={style.name}>Tatz</div>
+            <a
+              className="text-[#1868b7]"
+              href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/43617070992181872498657409852160512582181349889597339264956620564284712157185"
+            >
+              Tathagat
+            </a>
+          </div>
+        </div>
+      </div>
+    </Container>
+  )
 }
 
 export default Hero
