43c98ee87a7793c485a4f8da9d07a2549c69da45
Tamal Patra <patra.tamal@gmail.com>
2022-04-29 15:55:54 +0530

    refactoring, update components - Footer - utils

--- components/utils.ts
+++ components/utils.ts
@@ -1,48 +1,34 @@
+import path from 'path'
 
+export const ellipseAddress = (address: string, width = 5) =>
+  address && address.length > 2 * width
+    ? `${address.slice(0, width)}...${address.slice(-width)}`
+    : address
 
-export function ellipseAddress(address = '', width = 5) {
-    if (!address) {
-        return ''
-    }
-    return `${address.slice(0, width)}...${address.slice(-width)}`
+export const maskPhone = (address: string, width = 5) =>
+  address && address.length > width + 2
+    ? `${address.slice(0, width)}xxx${address.slice(-2)}`
+    : address
+
+export const ellipseName = (address: string, width = 15) =>
+  address && address.length > width ? `${address.slice(0, width)}...` : address
+
+export const rounded = (balance: string, length = 6) =>
+  balance && balance.length > length ? balance.slice(0, length) : balance
+
+const _baseUrl = (chainId?: number) => {
+  switch (chainId) {
+    case 80001:
+      return 'https://mumbai.polygonscan.com/'
+    case 4:
+      return 'https://rinkeby.etherscan.io/'
+    default:
+      throw Error(`value: ${chainId} is not a valid chainId`)
+  }
 }
 
-export function maskPhone(address = '', width = 5) {
-    if (!address) {
-        return ''
-    }
-    return `${address.slice(0, width)}xxx${address.slice(-2)}`
-}
+export const createUrlToken = (address = '', chainId?: number) =>
+  path.join(_baseUrl(chainId), 'token', address)
 
-export function ellipseName(address = '', width = 15) {
-    if (!address) {
-        return ''
-    }
-    return `${address.slice(0, width)}...`
-}
-export function rounded(balance = '', length = 6) {
-
-    return `${balance.slice(0, length)}`
-}
-
-export function createUrltoken(address = '', chainId = '') {
-    let baseUrl;
-    if (chainId === 80001) {
-        baseUrl = 'https://mumbai.polygonscan.com/token/'
-    }
-    if (chainId === 4) {
-        baseUrl = 'https://rinkeby.etherscan.io/token/'
-    }
-    return `${baseUrl}${address}`
-}
-
-export function createUrlAddress(address = '', chainId = '') {
-    let baseUrl;
-    if (chainId === 80001) {
-        baseUrl = 'https://mumbai.polygonscan.com/address/'
-    }
-    if (chainId === 4) {
-        baseUrl = 'https://rinkeby.etherscan.io/address/'
-    }
-    return `${baseUrl}${address}`
-}
+export const createUrlAddress = (address = '', chainId?: number) =>
+  path.join(_baseUrl(chainId), 'address', address)
