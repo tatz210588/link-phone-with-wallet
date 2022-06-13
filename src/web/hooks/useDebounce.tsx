48418471f57d397aabdb35a5da3de67ea7c270fd
Tamal Patra <patra.tamal@gmail.com>
2022-06-13 21:35:23 +0530

    `useDebounce` update

--- src/web/hooks/useDebounce.tsx
+++ src/web/hooks/useDebounce.tsx
@@ -1,22 +1,26 @@
-import { useDebugValue, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const useDebounce = (
-  callback: (...args: any[]) => any | void,
+  callback: (...args: any[]) => any | void | Promise<any>,
   delay: number,
-  debug: false
+  debug = false
 ) => {
   let [timer, setTimer] = useState(null)
 
   const debounce = async (...args: any[]) => {
     clearTimeout(timer)
-    setTimer(setTimeout(() => callback(...args), delay))
+    setTimer(
+      setTimeout(() => {
+        callback(...args)
+        debug && console.info({ args, callback, delay, timer })
+      }, delay)
+    )
   }
 
   useEffect(() => {
     return () => clearTimeout(timer)
   }, [])
 
-  debug && useDebugValue({ callback, delay, timer })
   return debounce
 }
 
